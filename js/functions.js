e03cfcce23f55afee34618a9d9e715e9a62d7941
Ionică Bizău <bizauionica@yahoo.com>
2014-01-14 22:01:41 +0200

    Create dynamic elements from js file

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    // TODO
+
+    // set an interval for creating helium elements
+    setInterval(function () {
+
+        // create helium element
+        var $helium = $("<div class=\"helium\"></div>");
+
+        // append the helium to screen
+        $(".screen").append($helium);
+
+        // and animate it
+        $(".helium").johnnysPath({d: 700}, [{x: 600}]);
+    }, 300);
 });
