2f0458b4e7a6b809e2228cbcd2aad8b67e315fb4
Ionică Bizău <bizauionica@yahoo.com>
2014-01-15 10:12:10 +0200

    FIxed the problem with johnny's path call.

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,38 @@
+var animate = true;
 $(document).ready(function () {
 
     $(".source").draggable({
         axis: "y",
-        containment: "parent"
+        containment: "parent",
+        drag: function () {
+            console.log($(".source").position().top);
+        }
     });
 
     // set an interval for creating helium elements
     setInterval(function () {
 
+        var y = $(".source").position().top + 6;
+
         // create helium element
         var $helium = $("<div class=\"helium\"></div>")
-                            .css("top", $(".source").position().top + 6);
+                            .css("top", y);
 
         // append the helium to screen
         $(".screen").append($helium);
 
+        if (y > 90) {
+            y = 10;
+        }
+
+        if (!animate) { return; }
         // and animate it
-        $(".helium").johnnysPath({d: 1000}, [{x: 600}], function () {
+        $helium.johnnysPath({d: 1000}, [
+            {x: 405},
+            {x: 600, y: y},
+        ], function () {
             $(this).remove();
         });
 
-    }, 700);
+    }, 300);
 });
