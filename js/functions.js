5b850c135d909a6b020d87ab5dea836d4f924dab
Ionică Bizău <bizauionica@yahoo.com>
2014-01-15 12:38:25 +0200

    Initial release

--- js/functions.js
+++ js/functions.js
@@ -11,15 +11,19 @@ $(document).ready(function () {
     // set an interval for creating helium elements
     setInterval(function () {
 
-        var y = $(".source").position().top + 6;
+        var y = $(".source").position().top + 6
+          , x = 600;
 
         // create helium element
         var $helium = $("<div class=\"helium\"></div>")
                             .css("top", y);
 
-        // top
-        if (y > 87 && y < 128) {
-
+        // don't compute anything in this case
+        if (y > 132 && y < 159) {
+            x = -10;
+            y += 20;
+        } else if (y > 87 && y < 160) {
+            
             // get delta
             var delta = 110 - y
 
@@ -28,7 +32,8 @@ $(document).ready(function () {
 
             // compute the new y
             y = 150 * percent/100;
-        } else if (y > 160 && y <= 210) {
+
+        } else if (y >= 160 && y < 210) {
 
             // get delta
             var delta = 65 - y + 160
@@ -38,7 +43,8 @@ $(document).ready(function () {
 
             // compute the new y
             y = 150 + 150 * percent/100;
-        } else if (y > 220) {
+
+        } else if (y >= 210) {
             y = y;
         }
 
@@ -47,8 +53,8 @@ $(document).ready(function () {
 
         // and animate it
         $helium.johnnysPath({d: 1000}, [
-            {x: 405},
-            {x: 600, y: y, d: 550},
+            {x: 385},
+            {x: x, y: y, d: 550},
         ], function () {
             $(this).remove();
         });
