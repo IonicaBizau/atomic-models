c71c1496cea63877ca077a1ab90fbad35d9eda4b
Ionică Bizău <bizauionica@yahoo.com>
2014-01-15 06:49:22 +0200

    Added jquery ui. Drag the source.

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
 
+    $(".source").draggable({
+        axis: "y",
+        containment: "parent"
+    });
+
     // set an interval for creating helium elements
     setInterval(function () {
 
         // create helium element
-        var $helium = $("<div class=\"helium\"></div>");
+        var $helium = $("<div class=\"helium\"></div>")
+                            .css("top", $(".source").position().top + 6);
 
         // append the helium to screen
         $(".screen").append($helium);
 
         // and animate it
-        $(".helium").johnnysPath({d: 700}, [{x: 600}]);
-    }, 300);
+        $(".helium").johnnysPath({d: 1000}, [{x: 600}]);
+
+    }, 700);
 });
